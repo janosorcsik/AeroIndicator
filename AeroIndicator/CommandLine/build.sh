5c11c328a04f76b569753fa958f5d4b4a68f25a4
RieN 7z <zrien7@gmail.com>
2025-03-12 16:50:33 +0800

    fix: support x86

--- AeroIndicator/CommandLine/build.sh
+++ AeroIndicator/CommandLine/build.sh
@@ -1,3 +1,6 @@
 #/bin/sh
 
-swiftc main.swift ../Socket.swift -o aeroIndicator
+swiftc -target arm64-apple-macos12 main.swift ../Socket.swift -o aeroIndicator_arm64
+swiftc -target x86_64-apple-macos12 main.swift ../Socket.swift -o aeroIndicator_x86_64
+
+lipo -create -output aeroIndicator_universal aeroIndicator_x86_64 aeroIndicator_arm64
